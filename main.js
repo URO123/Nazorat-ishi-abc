04f7d29510bf604e61987760bffa3e9e3c471579
unknown <uro123uzb@gmail.com>
2024-03-28 15:31:23 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,75 @@
+// -----------------------------------------1-topshiriq-----------------------------------------
+// let a = +prompt('1-Sonni kirit' , 0);
+// let b = +prompt('2-Sonni kirit' , 1);
+// let c = +prompt('3-Sonni kirit' , 2);
+
+// let d = function () {
+//     if(a>c&&a<b){
+//         return(`${a}-soni ortancha`);
+//     }
+//     if(b>c&&b<a){
+//         return(`${b}-soni ortancha`);
+//     }
+//     if(c>a&&c<b){
+//         return(`${c}-soni ortancha`);
+//     }
+//     if(a<c&&a>b){
+//         return(`${a}-soni ortancha`);
+//     }
+//     if(b<c&&b>a){
+//         return(`${b}-soni ortancha`);
+//     }
+//     if(c<a&&c>b){
+//         return(`${c}-soni ortancha`);
+//     }
+// };
+//     console.log(d());
+// -----------------------------------------2-topshiriq-----------------------------------------
+// function minusOrPlus(arr) {
+//     let b = 0
+//     let c = 0
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] > 0) {
+//             b++
+//         }
+//         if (arr[i] < 0) {
+//             c++
+//         }
+//     }
+//     return b + " ta musbat" + " " + c + " ta manfiy";
+// }
+
+// console.log(minusOrPlus([3, -9, 2, 0]));
+// console.log(minusOrPlus([-5, -7, -1, 0, 8, 9, 23]));
+// console.log(minusOrPlus([2, 6, 8, -1]));
+// console.log(minusOrPlus([5, 7, 9, 8]));
+// console.log(minusOrPlus([9, 8, 4, 0, -1, -2, 0, 5]));
+// -----------------------------------------3-topshiriq-----------------------------------------
+// function minusPluser(arr) {
+//     let a = 0
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] < 0) {
+//             a = a + arr[i]
+//         }
+//     }
+//     return a;
+// }
+
+// console.log(minusPluser([-1, -5, 6, -4, 8]));
+// console.log(minusPluser([6, -7, -9, 1, 0]));
+// console.log(minusPluser([4, 76, 56, 78]));
+// -----------------------------------------4-topshiriq-----------------------------------------
+// function maxerReturn(arr) {
+//     let maxer = 0
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] >= maxer) {
+//             maxer = arr[i]
+//         }
+//     }
+//     return maxer;
+// }
+
+// console.log(maxerReturn([1, 2, 3, 4, 5]));
+// console.log(maxerReturn([7, 9, 14, -5, 6]));
+// console.log(maxerReturn([8, -9, 45, 45, 23]));
+// console.log(maxerReturn([3, 3, 3]));
